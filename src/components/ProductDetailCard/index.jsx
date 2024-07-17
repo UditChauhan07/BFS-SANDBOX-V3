fcbae585c6683888a3121310239009f175db1c16
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-07-17 11:36:06 +0530

    17.07.24.02

--- src/components/ProductDetailCard/index.jsx
+++ src/components/ProductDetailCard/index.jsx
@@ -35,27 +35,27 @@ const ProductDetailCard = ({ product, orders, onPriceChangeHander = null, onQuan
   return (
     <div className="container mt-4 product-card-element">
       <div className="d-flex">
-        <div className={`${Styles.productimage} col-4`} style={{flex:'40% 1'}}>
+        <div className={`${Styles.productimage} col-4`} style={{ flex: '40% 1' }}>
           {product?.data?.imgSrc?.length > 0 ? <Slider data={product?.data?.imgSrc} /> : <Slider data={fakeProductSlider} />}
         </div>
-        <div className="col-8 ml-4 product-card-element-holder" style={{flex:'60% 1'}}>
+        <div className="col-8 ml-4 product-card-element-holder" style={{ flex: '60% 1' }}>
           <p style={{ textAlign: "start" }}>
-            <b>By</b>, <Link to={'/Brand/'+product.data.ManufacturerId__c} style={{color:'#000'}}><b>{product?.data?.ManufacturerName__c}</b></Link>
+            <b>BY</b>, <Link to={'/Brand/' + product.data.ManufacturerId__c} className={Styles.brandHolder}><b>{product?.data?.ManufacturerName__c}</b></Link>
           </p>
-          <h2 style={{ textAlign: "start" }}>
-            <b>{product?.data?.Name}</b>
+          <h2 className={Styles.nameHolder}>
+            {product?.data?.Name}
           </h2>
           {product?.discount ? (
             <p style={{ textAlign: "start", color: "black" }}>
               <span className={Styles.crossed}>{product?.data?.usdRetail__c}</span>&nbsp;${salesPrice}
             </p>
           ) : (
-            <p style={{ textAlign: "start" }}>
+            <p className={Styles.priceHolder}>
               <b>{product?.data?.usdRetail__c}</b>
             </p>
           )}
           {product?.data?.Description && (
-            <p style={{ textAlign: 'start', color: "#898989" }}>
+            <p className={Styles.descHolder}>
               {product.data.Description.length > 750 ? (
                 isDescriptionExpanded ? (
                   product.data.Description
@@ -70,28 +70,6 @@ const ProductDetailCard = ({ product, orders, onPriceChangeHander = null, onQuan
                   {isDescriptionExpanded ? 'Learn Less' : 'Learn More'}
                 </button>
               )}
-            </p>
-          )}
-          {/* {product?.data?.Description && <p style={{ textAlign: 'start', color: "#898989" }}>{product?.data?.Description}</p>} */}
-          <hr style={{ borderTop: "1px dashed ", fontSize: "20px", color: "black" }}></hr>
-          {product?.data?.ProductCode && <p style={{ textAlign: "start", color: "#898989" }}>
-            Product Code: <b style={{ color: "black" }}>{product?.data?.ProductCode}</b>
-          </p>}
-          {product?.data?.ProductUPC__c && <p style={{ textAlign: "start", color: "#898989" }}>
-            Product UPC: <b style={{ color: "black" }}>{product?.data?.ProductUPC__c}</b>
-          </p>}
-          {product?.data?.Min_Order_QTY__c &&
-            <p style={{ textAlign: "start", color: "#898989" }}>
-              Min Order QTY: <b style={{ color: "black" }}>{product?.data?.Min_Order_QTY__c}</b>
-            </p>}
-          {product?.data?.Category__c && (
-            <p style={{ textAlign: "start", color: "#898989" }}>
-              Category: <b style={{ color: "black" }}>{product?.data?.Category__c}</b>
-            </p>
-          )}
-          {product.data?.Collection__c && (
-            <p style={{ textAlign: "start", color: "#898989" }}>
-              Collection: <b style={{ color: "black" }}>{product.data?.Collection__c}</b>
             </p>
           )}
           {isAddtoCart && product?.discount ? (
@@ -149,110 +127,141 @@ const ProductDetailCard = ({ product, orders, onPriceChangeHander = null, onQuan
               </>
             </>
           ) : (
-            <div className="d-flex gap-4">
+            <div className="d-flex align-items-center gap-4 h-[5rem] ">
               {toRedirect && <Link to={toRedirect} className={Styles.button}>
                 Add to cart
               </Link>}
             </div>
           )}
+          {/* {product?.data?.Description && <p style={{ textAlign: 'start', color: "#898989" }}>{product?.data?.Description}</p>} */}
+          <hr style={{ borderTop: "3px dashed #000", fontSize: "20px", color: "black" }}></hr>
+          {product?.data?.ProductCode && <p className={Styles.descHolder}>
+            Product Code: <span >{product?.data?.ProductCode}</span>
+          </p>}
+          {product?.data?.ProductUPC__c && <p className={Styles.descHolder}>
+            Product UPC: <span >{product?.data?.ProductUPC__c}</span>
+          </p>}
+          {product?.data?.Min_Order_QTY__c &&
+            <p className={Styles.descHolder}>
+              Min Order QTY: <span >{product?.data?.Min_Order_QTY__c}</span>
+            </p>}
+          {product?.data?.Category__c && (
+            <p className={Styles.descHolder}>
+              Category: <span >{product?.data?.Category__c}</span>
+            </p>
+          )}
+          {product.data?.Collection__c && (
+            <p className={Styles.descHolder}>
+              Collection: <span >{product.data?.Collection__c}</span>
+            </p>
+          )}
         </div>
       </div>
       <hr></hr>
       {product.data?.Full_Product_Description__c && product.data?.Full_Product_Description__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Full Product Description:</b> {product.data?.Full_Product_Description__c}
+        <p className={Styles.descHolder}>
+          <span >Full Product Description:</span> {product.data?.Full_Product_Description__c}
         </p>
       )}
       {product.data?.Desired_Result__c && product.data?.Desired_Result__c != "N/A" && (
         <div
-          style={{ textAlign: "start", color: "#898989" }}
-          dangerouslySetInnerHTML={{ __html: `Desired Result: ${product.data?.Desired_Result__c}` }}
+          className={Styles.descHolder}
+          dangerouslySetInnerHTML={{ __html: `<span>Desired Result:</span> ${product.data?.Desired_Result__c}` }}
         />
       )}
       {product.data?.Key_Ingredients__c && product.data?.Key_Ingredients__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Key Ingredients:</b> {product.data?.Key_Ingredients__c}
+        <p className={Styles.descHolder}>
+          <span>Key Ingredients:</span> {product.data?.Key_Ingredients__c}
         </p>
       )}
       {product.data?.Full_Ingredients_List__c && product.data?.Full_Ingredients_List__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
+        <p className={Styles.descHolder}>
           {" "}
-          <b style={{ color: "black" }}>Ingredients List:</b> {product.data?.Full_Ingredients_List__c}
+          <span >Ingredients List:</span> {product.data?.Full_Ingredients_List__c}
         </p>
       )}
       {product.data?.Size_Volume_Weight__c && product.data?.Size_Volume_Weight__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Size (Volume/Weight):</b> {product.data?.Size_Volume_Weight__c}
+        <p className={Styles.descHolder}>
+          <span >Size (Volume/Weight):</span> {product.data?.Size_Volume_Weight__c}
         </p>
       )}
       {product.data?.Skin_Tone__c && product.data?.Skin_Tone__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Skin Tone:</b> {product.data?.Skin_Tone__c}
+        <p className={Styles.descHolder}>
+          <span >Skin Tone:</span> {product.data?.Skin_Tone__c}
         </p>
       )}
       {product.data?.Skin_Type__c && product.data?.Skin_Type__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Skin Type:</b> {product.data?.Skin_Type__c}
+        <p className={Styles.descHolder}>
+          <span >Skin Type:</span> {product.data?.Skin_Type__c}
         </p>
       )}
       {product.data?.Travel_or_Full_Size__c && product.data?.Travel_or_Full_Size__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Product Size:</b> {product.data?.Travel_or_Full_Size__c}
+        <p className={Styles.descHolder}>
+          <span>Product Size:</span> {product.data?.Travel_or_Full_Size__c}
         </p>
       )}
       {product?.data?.Newness_Alias__c && product.data?.Newness_Alias__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Product Newness Name:</b> {product?.data?.Newness_Alias__c}
+        <p className={Styles.descHolder}>
+          <span>Product Newness Name:</span> {product?.data?.Newness_Alias__c}
         </p>
       )}
       {product?.data?.Newness_Start_Date__c && product.data?.Newness_Start_Date__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Product Newness Start Date:</b> {product?.data?.Newness_Start_Date__c}
+        <p className={Styles.descHolder}>
+          <span>Product Newness Start Date:</span> {product?.data?.Newness_Start_Date__c}
         </p>
       )}
       {product?.data?.Newness_Report_End_Date__c && product.data?.Newness_Report_End_Date__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Product Newness End Date:</b> {product?.data?.Newness_Report_End_Date__c}
+        <p className={Styles.descHolder}>
+          <span>Product Newness End Date:</span> {product?.data?.Newness_Report_End_Date__c}
         </p>
       )}
       {product?.data?.Season__c && product.data?.Season__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Season:</b> {product?.data?.Season__c},
+        <p className={Styles.descHolder}>
+          <span>Season:</span> {product?.data?.Season__c},
         </p>
       )}
       {product?.data?.CreatedDate && product.data?.CreatedDate != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Create Date:</b> {new Date(product?.data?.CreatedDate).toDateString()}
+        <p className={Styles.descHolder}>
+          <span>Create Date:</span> {new Date(product?.data?.CreatedDate).toDateString()}
         </p>
       )}
       {product?.data?.Launch_Date__c && product.data?.Launch_Date__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Launch Date:</b> {DateConvert(product?.data?.Launch_Date__c)}
+        <p className={Styles.descHolder}>
+          <span >Launch Date:</span> {DateConvert(product?.data?.Launch_Date__c)}
         </p>
       )}
       {product?.data?.Ship_Date__c && product.data?.Ship_Date__c != "N/A" && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
-          <b style={{ color: "black" }}>Ship Date:</b> {DateConvert(product?.data?.Ship_Date__c)}
+        <p className={Styles.descHolder}>
+          <span>Ship Date:</span> {DateConvert(product?.data?.Ship_Date__c)}
         </p>
       )}
       {/* <p style={{ textAlign: 'start' }}>Product Edit Date: {new Date(product?.data?.LastModifiedDate).toDateString()},</p> */}
       {(product.data?.Point_of_difference_1__c || product.data?.Point_of_difference_2__c || product.data?.Point_of_difference_3__c) && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
+        <p className={Styles.descHolder}>
+          <span>Point of difference:</span>
           <ol>
-            {product.data?.Point_of_difference_1__c && <li>Point of difference #1: {product.data?.Point_of_difference_1__c}</li>}
-            {product.data?.Point_of_difference_2__c && <li>Point of difference #2: {product.data?.Point_of_difference_2__c}</li>}
-            {product.data?.Point_of_difference_3__c && <li>Point of difference #3: {product.data?.Point_of_difference_3__c}</li>}
+            {product.data?.Point_of_difference_1__c && <li><span>#1:</span> {product.data?.Point_of_difference_1__c}</li>}
+            {product.data?.Point_of_difference_2__c && <li><span>#2:</span> {product.data?.Point_of_difference_2__c}</li>}
+            {product.data?.Point_of_difference_3__c && <li><span>#3:</span> {product.data?.Point_of_difference_3__c}</li>}
           </ol>
         </p>
       )}
       {product.data?.Usage_and_Application_Tips__c && (
-        <p style={{ textAlign: "start", color: "#898989" }}>
+        <p className={Styles.descHolder}>
+          <span>Usage and Application Tips:</span>
           {product.data?.Usage_and_Application_Tips__c}
-          <ol>
-            {product.data?.Use_it_with_Option_1__c && <li>Use it with (Option #1): {product.data?.Use_it_with_Option_1__c}</li>}
-            {product.data?.Use_it_with_Option_2__c && <li>Use it with (Option #2): {product.data?.Use_it_with_Option_2__c}</li>}
-            {product.data?.Use_it_with_Option_3__c && <li>Use it with (Option #3): {product.data?.Use_it_with_Option_3__c}</li>}
-          </ol>
+          {product.data?.Use_it_with_Option_1__c || product.data?.Use_it_with_Option_2__c || product.data?.Use_it_with_Option_3__c ?
+            <>
+              <br />
+              <br />
+              <span>Use it with</span>
+              <ol>
+                {product.data?.Use_it_with_Option_1__c && <li><span>#1:</span> {product.data?.Use_it_with_Option_1__c}</li>}
+                {product.data?.Use_it_with_Option_2__c && <li><span>#2:</span> {product.data?.Use_it_with_Option_2__c}</li>}
+                {product.data?.Use_it_with_Option_3__c && <li><span>#3:</span> {product.data?.Use_it_with_Option_3__c}</li>}
+              </ol>
+            </>
+            : <>{" "}</>}
         </p>
       )}
     </div>
