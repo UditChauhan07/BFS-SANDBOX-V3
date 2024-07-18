38444d6d7fba3a0869a2c0a7a8526e7a7350c32f
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-07-18 18:00:51 +0530

    18.07.24.01

---
+++ src/components/EmailBlasts/EmailReport.jsx
@@ -0,0 +1,124 @@
+import { useEffect, useMemo, useState } from "react";
+import AppLayout from "../AppLayout";
+import { GetAuthData, getEmailBlastReport } from "../../lib/store";
+import SettingNotify from "./SettingNotify";
+import { BiBoltCircle } from "react-icons/bi";
+import Styles from "./index.module.css"
+import Loading from "../Loading";
+import ModalPage from "../Modal UI";
+import Pagination from "../Pagination/Pagination";
+
+const EmailReport = ({ setting, setSetting, onCheckout }) => {
+    const [emailReports, setEmailReport] = useState({ isLoad: false, data: [] })
+    const [showLayout, setShowLayout] = useState()
+    const [token, setToken] = useState();
+    const [searchValue, setSearchValue] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+    useEffect(() => {
+        getReportHandler();
+    }, [])
+
+    const getReportHandler = () => {
+        GetAuthData().then((user) => {
+            setToken(user.access_token)
+            getEmailBlastReport({ key: user.access_token, Id: user.Sales_Rep__c }).then((report) => {
+                setEmailReport({ isLoad: true, data: report })
+            }).catch((reportErr) => {
+                console.log({ reportErr });
+            })
+        }).catch((userErr) => {
+            console.log({ userErr });
+        })
+    }
+    const { isLoad, data } = emailReports;
+    const letterList = useMemo(() => {
+        return data
+        // return (
+        //     data
+        //         ?.filter((frame) => {
+        //             return (
+        //                 !searchValue?.length ||
+        //                 [
+        //                     frame.NotSend,
+        //                     frame.FailMail,
+        //                     frame.SendMail,
+        //                     frame.Total,
+        //                     frame.Year,
+        //                     frame.Month,
+        //                     frame.Day
+        //                 ].some(property => {
+        //                     const propertyValue = property?.toLowerCase();
+        //                     return propertyValue.includes(searchValue);
+        //                 })
+        //             );
+        //         })
+        // )
+    }, [data, searchValue]);
+    if (setting) {
+        return (<SettingNotify setSetting={setSetting} />)
+    } else {
+        return (
+            <>
+                <ModalPage
+                    open={showLayout ?? false}
+                    content={<div style={{ width: '80vw' }} dangerouslySetInnerHTML={{ __html: showLayout }} />}
+                    onClose={() => setShowLayout()}
+                />
+                <div style={{ position: 'sticky', top: '150px', background: '#ffffff', padding: '2px 0', zIndex: 1 }}>
+                    <div className={Styles.titleHolder} style={{ marginBottom: '0px' }}>
+                        <h2>NewLetter</h2>
+                        <div className="d-flex">
+                            <div className={`${Styles.settingButton}  d-flex  justify-content-center align-items-center`} onClick={() => { setSetting(true) }}>
+                                <BiBoltCircle />&nbsp;Setting
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-end align-items-center" style={{ margin: '15px 0 27px 0' }}>
+
+                        <input type="text" autoComplete="off" id="newletterSearch" title="search for newletter" placeholder="Search newletter" className={Styles.searchBox} onKeyUp={(e) => { setSearchValue(e.target.value); setCurrentPage(1) }} />
+                    </div>
+                </div>
+                {isLoad ? <table style={{ width: '100%' }}>
+                    <thead className={Styles.table} style={{ position: 'sticky', top: '265px', zIndex: 11 }}>
+                        <tr>
+                            <th style={{ width: '200px' }}>Date</th>
+                            <th style={{ width: '200px' }}>Layout</th>
+                            <th>Total Mail</th>
+                            <th>Send Mails</th>
+                            <th>Fail Mails</th>
+                            <th>Not Mails</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {letterList.length > 0 ? letterList.slice(
+                            (currentPage - 1) * 10,
+                            currentPage * 10
+                        ).map((frame, index) => {
+                            return (
+                                <tr key={index} className={Styles.tableRow}>
+                                    <td style={{ width: '200px' }}>
+                                        <p style={{ textDecoration: 'underline' }} onClick={() => { onCheckout({ day: frame.Day, month: frame.MonthValue, year: frame.Year }) }}>
+                                            {frame.Month} {frame.Day}, {frame.Year}
+                                        </p>
+                                    </td>
+                                    <td style={{ width: '200px' }}><p title="click to see Layout of this newletter" onClick={() => { setShowLayout(frame?.FirstBody.replaceAll("{{token}}", token)) }} style={{ cursor: 'pointer' }}>click to see Layout</p></td>
+                                    <td>{frame.Total}</td>
+                                    <td>{frame.SendMail}</td>
+                                    <td>{frame.FailMail}</td>
+                                    <td>{frame.NotSend}</td>
+                                </tr>
+                            )
+                        }) : <tr className="text-center" style={{ height: '200px' }}><td colSpan={6}>No data found.</td></tr>}
+                    </tbody>
+                    {!setting && <Pagination
+                        className="pagination-bar"
+                        currentPage={currentPage}
+                        totalCount={letterList.length}
+                        pageSize={10}
+                        onPageChange={(page) => setCurrentPage(page)}
+                    />}
+                </table> : <Loading height={'50vh'} />}
+            </>)
+    }
+}
+export default EmailReport;
--- src/pages/EmailSetting.jsx
+++ src/pages/EmailSetting.jsx
@@ -1,114 +1,41 @@
 import { useEffect, useMemo, useState } from "react"
 import AppLayout from "../components/AppLayout"
 import { DateConvert, GetAuthData, admins, getEmailBlast, months, sortArrayHandler } from "../lib/store";
-import { useNavigate } from "react-router-dom";
-import Pagination from "../components/Pagination/Pagination";
 import EmailTable from "../components/EmailBlasts/EmailTable";
 import Loading from "../components/Loading";
 import { FilterItem } from "../components/FilterItem";
 import { CloseButton, SearchIcon } from "../lib/svg";
-let PageSize = 10;
+import EmailReport from "../components/EmailBlasts/EmailReport";
+
 const EmailSetting = () => {
-    const navigate = useNavigate();
-    const [user, setUser] = useState([]);
-    const [contactList, setContactList] = useState({ isLoaded: false, data: [] })
-    const [notifyDate, setNotifyDate] = useState([])
-    const [currentPage, setCurrentPage] = useState(1);
+
     const [setting, setSetting] = useState(false)
-    const [checkId, setCheckId] = useState([])
-    const [searchValue, setSearchValue] = useState();
-    const [checked, setChecked] = useState(false)
-    const [filter, setFilter] = useState({});
     const [monthList, setMonthList] = useState([]);
     const [dayList, setDayList] = useState([])
     const [month, setMonth] = useState();
     const [day, setDay] = useState();
+    const [year, setYear] = useState();
+    const [filter, setFilter] = useState({});
 
-    useEffect(() => {
-        getDataHandler()
-    }, [])
-    const getDataHandler = (reset=false) => {
-        setSearchValue(null)
-        setCheckId([])
-        setNotifyDate([])
-        GetAuthData().then((user) => {
-            if (admins.includes(user.Sales_Rep__c)) {
-                setUser(user)
-                getEmailBlast({ key: user.access_token, Id: user.Sales_Rep__c, month:reset?null:month, day:reset?null:day }).then((list) => {
-                    let contactList = sortArrayHandler(JSON.parse(list.notifyMail || "[]") || [], g => g?.updatedAt, 'desc')
-                    setContactList({ isLoaded: true, data: contactList })
-                    setNotifyDate(list.notifyDate)
-                    let monthList = Object.keys(list.filter);
-                    setMonthList(monthList)
-                    setDayList(list.filter[months[list.month-1]]||[]);
-                    setMonth(list.month)
-                    setDay(list.day)
-                    setFilter(list.filter)
-                }).catch((conErr) => {
-                    console.log({ conErr });
-                })
-            } else {
-                navigate('/dashboard')
-            }
-        }).catch((err) => {
-            console.log({ err });
-        })
-    }
-    const { isLoaded, data } = contactList||false
-    const mailList = useMemo(() => {
-        return (
-            data
-                ?.filter((contact) => {
-                    return (
-                        !searchValue?.length ||
-                        [
-                            contact.Account,
-                            contact.Brands,
-                            contact.ContactEmail,
-                            contact.ContactName,
-                            DateConvert(contact.Date, true)
-                        ].some(property => {
-                            const propertyValue = property?.toLowerCase();
-                            return propertyValue.includes(searchValue);
-                        })
-                    );
-                })
-        );
-    }, [data, searchValue]);
-    function checkedAll(value) {
-        setChecked(!checked)
-        let temp = []
-        if (value) {
-            mailList.map((contact, index) => {
-                temp.push(contact.id)
-            })
-        }
-        setCheckId(temp)
-    }
-    // return <AppLayout/>;
-    const monthChangeHandler= (value)=>{
+    const monthChangeHandler = (value) => {
         setMonth(value)
         setDay(null)
-        if(filter[months[value-1]]){
-            if(filter[months[value-1]].length==1){
-                setDay(filter[months[value-1]][0])
+        if (filter[months[value - 1]]) {
+            if (filter[months[value - 1]].length == 1) {
+                setDay(filter[months[value - 1]][0])
             }
         }
-        setDayList(filter[months[value-1]]||[]);
+        setDayList(filter[months[value - 1]] || []);
     }
-    const reset = ()=>{
-        setDayList([]);
-        setMonthList([])
-        setDay();
-        setMonth();
-        setFilter({})
-        setContactList({ isLoaded: false, data: [] })
-        getDataHandler(true)
+    const getDetailsOfNewLetter = ({ year, month, day }) => {
+        setDay(day)
+        setMonth(month)
+        setYear(year)
     }
-
     return (<AppLayout
         filterNodes={
             <>
+            {monthList.length?
                 <FilterItem
                     minWidth="220px"
                     label="month"
@@ -116,10 +43,11 @@ const EmailSetting = () => {
                     value={month}
                     options={monthList.map((month) => ({
                         label: month,
-                        value: (months.indexOf(month)+1),
+                        value: (months.indexOf(month) + 1),
                     }))}
                     onChange={(value) => monthChangeHandler(value)}
-                />
+                />:null}
+                {dayList.length?
                 <FilterItem
                     minWidth="220px"
                     label="Day"
@@ -130,35 +58,18 @@ const EmailSetting = () => {
                         value: month,
                     }))}
                     onChange={(value) => setDay(value)}
-                />
-                <button onClick={() => {setContactList({ isLoaded: false, data: [] });getDataHandler()}} className="border px-2 py-1 leading-tight d-grid"> <SearchIcon fill="#fff" width={20} height={20} />
+                />:null}
+                {(dayList.length && monthList.length)?
+                <button className="border px-2 py-1 leading-tight d-grid"> <SearchIcon fill="#fff" width={20} height={20} />
                     <small style={{ fontSize: '6px', letterSpacing: '0.5px', textTransform: 'uppercase' }}>search</small>
-                </button>
-                <div className="d-flex gap-3">
-              <button className="border px-2 py-1 leading-tight d-grid" onClick={()=>{reset();}}>
-                <CloseButton crossFill={'#fff'} height={20} width={20} />
-                <small style={{ fontSize: '6px', letterSpacing: '0.5px', textTransform: 'uppercase' }}>clear</small>
-              </button>
-            </div>
+                </button>:null}
             </>
         }
     >
-        {isLoaded ? <div>
-            <div style={{ width: '80%', margin: 'auto' }}>
-                <EmailTable data={mailList.slice(
-                    (currentPage - 1) * PageSize,
-                    currentPage * PageSize
-
-                )} setSetting={setSetting} setting={setting} user={user} setSearchValue={setSearchValue} checkIdObj={{ setCheckId, checkId, checkedAll, checked }} notifyDate={notifyDate} getDataHandler={getDataHandler} setCurrentPage={setCurrentPage} setContactList={setContactList} />
-                {!setting && <Pagination
-                    className="pagination-bar"
-                    currentPage={currentPage}
-                    totalCount={mailList.length}
-                    pageSize={PageSize}
-                    onPageChange={(page) => setCurrentPage(page)}
-                />}
-            </div>
-        </div> : <Loading height={'60vh'} />}
+        <div style={{ width: '80%', margin: 'auto',minHeight:'500px' }}>
+            {(day && month && year) ?
+                <EmailTable setDayList={setDayList} setMonthList={setMonthList} day={day} month={month} year={year} setFilter={setFilter} setYear={setYear} setDay={setDay} setMonth={setMonth} /> : <EmailReport setSetting={setSetting} setting={setting} onCheckout={getDetailsOfNewLetter} />}
+        </div>
     </AppLayout>)
 }
 export default EmailSetting
