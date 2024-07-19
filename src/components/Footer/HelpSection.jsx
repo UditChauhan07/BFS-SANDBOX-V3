28a5a94d9e47cc9c1071859f1ef5c7e091cd0935
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-07-19 18:55:12 +0530

    19.07.24.08

--- src/components/Footer/HelpSection.jsx
+++ src/components/Footer/HelpSection.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import styles from "../All Headers/header/index.module.css";
-import footerStyle from "./index.module.css";
-import topNavStyle from "../All Headers/topNav/index.module.css";
-import { Link } from "react-router-dom";
-const HelpSection = () => {
-  return (
-    <div className="mt-3 d-none-print">
-      <div style={{ backgroundColor: "#F3E8E0" }} className=" ">
-        <div  className="  justify-content-center  align-items-center ">
-          <div className={footerStyle.ControlHelp}>
-          <p className={`m-0  ${footerStyle.textLarge}`}>Help us Improve</p>
-          <p className={`m-0 ${topNavStyle.language}`}>Take a brief survey about today's visit</p>
-          <p className={`m-0  ${topNavStyle.language} ${footerStyle.underline}`}><Link style={{color:'#000'}} to="/lead">Begin Survey</Link></p>
-        </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// import React from "react";
+// import footerStyle from "./index.module.css";
+// import topNavStyle from "../All Headers/topNav/index.module.css";
+// import { Link } from "react-router-dom";
+// const HelpSection = () => {
+//   return (
+//     <div className="mt-3 d-none-print">
+//       <div style={{ backgroundColor: "#F3E8E0" }}>
+//         <div className="  justify-content-center  align-items-center ">
+//           <div className={footerStyle.ControlHelp}>
+//             <p className={`m-0  ${footerStyle.textLarge}`}>Help us Improve</p>
+//             <p className={`m-0 ${topNavStyle.language}`}>Take a brief survey about today's visit</p>
+//             <p className={`m-0  ${topNavStyle.language} ${footerStyle.underline}`}><Link style={{ color: '#000' }} to="/lead">Begin Survey</Link></p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default HelpSection;
+// export default HelpSection;
