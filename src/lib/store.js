47563080869fb01155c2c608cc8cb91e7848a526
GauravDx01 <gaurav01.dx@gmail.com>
2024-08-23 12:30:16 +0530

    dkfjj

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,6 @@
 // export const originAPi = process.env.REACT_APP_OA_URL || "https://temp.beautyfashionsales.com/"
 // export const originAPi = "https://dev.beautyfashionsales.com"
-// export const originAPi = "http://localhost:6194"
+// export const originAPi = "http://localhost:2619"
 export const originAPi = "https://bfs.uditchauhan.com"
 
 let url = `${originAPi}/beauty/`;
