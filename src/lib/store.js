44a6029929405241082d48aa768ead8c6d990c8e
Manisharanidx <manisharani.dx@gmail.com>
2024-09-13 11:42:09 +0530

    B2BCode

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,7 @@
 // export const originAPi = process.env.REACT_APP_OA_URL || "https://temp.beautyfashionsales.com/"
 // export const originAPi = "https://dev.beautyfashionsales.com"
-export const originAPi = "http://localhost:2611"
-// export const originAPi = "https://bfs.uditchauhan.com"
+// export const originAPi = "http://localhost:2611"
+export const originAPi = "https://bfs.uditchauhan.com"
 
 let url = `${originAPi}/beauty/`;
 let URL = `${originAPi}/beauty/0DS68FOD7s`;
